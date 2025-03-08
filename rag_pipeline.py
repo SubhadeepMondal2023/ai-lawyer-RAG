b72ac3952416c3dfa98d8b245606402cd1a41a47
Subha <msubhadeep9635@gmail.com>
2025-03-08 13:51:45 +0530

    final checks before deploying

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -34,6 +34,3 @@ def answer_query(documents, model, query):
     chain = prompt | model
     return chain.invoke({"question": query, "context": context})
 
-#question="If a government forbids the right to assemble peacefully which articles are violated and why?"
-#retrieved_docs=retrieve_docs(question)
-#print("AI Lawyer: ",answer_query(documents=retrieved_docs, model=llm_model, query=question))
